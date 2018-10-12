5fdaf6018d92c6becfa5aff47637312b056a3f05
道面 翔剛(業務委託) <ditsdome@mediba.jp>
2018-10-12 10:09:34 +0900

    新規作成

---
+++ sample/src/sample/Sample.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sample;
+
+/**
+ * @author ditsdome
+ *
+ */
+public class Sample {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+
+	}
+
+}
